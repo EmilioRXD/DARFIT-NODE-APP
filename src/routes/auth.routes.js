40edc19b0d2ed001557d17bf1152a3ee859ae3c2
EmilioRXD <jes99.444.rodriguez@gmail.com>
2022-08-27 22:00:59 -0400

    Actualización de Interfaz

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,23 +1,13 @@
 import { Router } from "express";
 import {
-  renderSignUpForm,
-  singup,
   renderSigninForm,
-  signin,
-  logout,
+  renderSignin
 } from "../controllers/auth.controllers.js";
 
 const router = Router();
 
 // Routes
-router.get("/auth/signup", renderSignUpForm);
-
-router.post("/auth/signup", singup);
-
 router.get("/auth/signin", renderSigninForm);
 
-router.post("/auth/signin", signin);
-
-router.get("/auth/logout", logout);
-
+router.post("/auth/signin", renderSignin);
 export default router;
